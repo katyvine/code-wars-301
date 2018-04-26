900920b5b62dd7d2f3939f7dd8ed6fd10a58b111
Katy Bowers <36514021+katyvine@users.noreply.github.com>
2018-04-25 21:54:24 -0700

    Update solution.js

--- Sum-Odd-Nums/solution.js
+++ Sum-Odd-Nums/solution.js
@@ -2,11 +2,12 @@ function cubeOdd(arr) {
   for (var i in arr){
     if (typeof arr[i] === 'string'){
     return undefined}
-    }
-   
+   }
+
     let cubed = arr.map(x => Math.pow(x,3))
     let filter = cubed.filter(z => (z %2===1 || z %2 ===-1))
+     if (filter.length<1){
+    return 0;}
     let total = filter.reduce((acc, cv) => acc + cv)
     
     return total ;
-}
